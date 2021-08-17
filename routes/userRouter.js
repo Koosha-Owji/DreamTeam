f010a07e63c7d7b2c23e4bec1b1c9f01055eec96
irelandn <irelandn@student.unimelb.edu.au>
2021-08-17 20:02:00 +1000

    Connection to MongoDB successful. Mock user Tash displays on userRoute /info/Tash

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,6 +6,10 @@ userRouter = express.Router()
 // require the user controller
 const userController = require('../controllers/userController.js')
 
+userRouter.get('/', (req, res) => {
+    res.send('info page')
+})
+
 // handle the GET request toget all users
 userRouter.get('/user-info', userController.getAllUsers)
 
