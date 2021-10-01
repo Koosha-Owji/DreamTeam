d45a1a54e8ac93429e072ef47db5da7f256f3203
Koosha-Owji <87961112+Koosha-Owji@users.noreply.github.com>
2021-10-01 22:54:38 +1000

    Update ContactLabel.component.js

--- frontend/src/components/contact/ContactLabel.component.js
+++ frontend/src/components/contact/ContactLabel.component.js
@@ -1,6 +1,6 @@
 
 import React, {Component} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+//import { makeStyles } from '@material-ui/core/styles';
 //import Typography from '@material-ui/core/Typography';
 import Chip from '@material-ui/core/Chip';
 import Grid from '@material-ui/core/Grid';
