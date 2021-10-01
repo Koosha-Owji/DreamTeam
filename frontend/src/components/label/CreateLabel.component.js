1af853bd784f0683f749f23bf18be48c9ae6ffc2
Koosha-Owji <koosha.owji@yahoo.com>
2021-10-01 22:50:27 +1000

    debugged label page

--- frontend/src/components/label/CreateLabel.component.js
+++ frontend/src/components/label/CreateLabel.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
